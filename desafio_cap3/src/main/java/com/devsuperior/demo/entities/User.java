bf8af25baee6b34885f6e603956f2c4f3ade5c81
bredaneves <bredaneves@gmail.com>
2026-04-06 23:18:21 -0300

    Cap3 - Desafio - User Service, Repository and Projection

--- desafio_cap3/src/main/java/com/devsuperior/demo/entities/User.java
+++ desafio_cap3/src/main/java/com/devsuperior/demo/entities/User.java
@@ -1,8 +1,12 @@
 package com.devsuperior.demo.entities;
 
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -17,7 +21,7 @@ import jakarta.persistence.Table;
 @SuppressWarnings("serial")
 @Entity
 @Table(name = "tb_user")
-public class User {
+public class User implements UserDetails {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -110,5 +114,17 @@ public class User {
 			return false;
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return roles;
+	}
+
+	@Override
+	public String getUsername() {		
+		return email;
 	}	
+	
+	
 }
