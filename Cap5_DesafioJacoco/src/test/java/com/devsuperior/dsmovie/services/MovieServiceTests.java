dfb6e6bcc3a61b302dadcf1993ed7cb3ca59759b
bredaneves <bredaneves@gmail.com>
2026-05-02 23:20:00 -0300

    Cap5 - Desafio Jacoco - User Testes OK

--- Cap5_DesafioJacoco/src/test/java/com/devsuperior/dsmovie/services/MovieServiceTests.java
+++ Cap5_DesafioJacoco/src/test/java/com/devsuperior/dsmovie/services/MovieServiceTests.java
@@ -1,15 +1,70 @@
 package com.devsuperior.dsmovie.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.devsuperior.dsmovie.dto.MovieDTO;
+import com.devsuperior.dsmovie.entities.MovieEntity;
+import com.devsuperior.dsmovie.repositories.MovieRepository;
+import com.devsuperior.dsmovie.tests.MovieFactory;
 
 @ExtendWith(SpringExtension.class)
 public class MovieServiceTests {
 	
 	@InjectMocks
 	private MovieService service;
+	
+	@Mock
+	private MovieRepository repository;
+	
+	private long existingMovieId, nonExistingMovieId, dependentMovieId;
+	private String movieName;
+	private MovieEntity movie;
+	private MovieDTO movieDTO;
+	private PageImpl<MovieEntity> page;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		existingMovieId = 1L;
+		nonExistingMovieId = 2L;
+		dependentMovieId = 3L;
+		
+		movieName = "Cisne Negro";
+		
+		movie = MovieFactory.createMovieEntity();
+		movieDTO = new MovieDTO(movie);
+		page = new PageImpl<>(List.of(movie));
+		Pageable pageable = PageRequest.of(0, 12);
+		
+		/*Mockito.when(repository.searchByTitle(movieName, pageable).thenReturn(Optional.of(movie));
+		
+		Mockito.when(repository.findById(nonExistingProductId)).thenReturn(Optional.empty());
+		
+		Mockito.when(repository.searchByName(any(), (Pageable)any())).thenReturn(page);
+		
+		Mockito.when(repository.save(any())).thenReturn(product);
+		
+		Mockito.when(repository.getReferenceById(existingProductId)).thenReturn(product);
+		Mockito.when(repository.getReferenceById(nonExistingProductId)).thenThrow(EntityNotFoundException.class);
+		
+		Mockito.when(repository.existsById(existingProductId)).thenReturn(true);
+		Mockito.when(repository.existsById(dependentProductId)).thenReturn(true);
+		Mockito.when(repository.existsById(nonExistingProductId)).thenReturn(false);
+		
+		Mockito.doNothing().when(repository).deleteById(existingProductId);
+		Mockito.doThrow(DataIntegrityViolationException.class).when(repository).deleteById(dependentProductId);*/
+	}
 	
 	@Test
 	public void findAllShouldReturnPagedMovieDTO() {
--- Cap5_DesafioJacoco/src/test/java/com/devsuperior/dsmovie/tests/UserFactory.java
+++ Cap5_DesafioJacoco/src/test/java/com/devsuperior/dsmovie/tests/UserFactory.java
@@ -5,7 +5,7 @@ import com.devsuperior.dsmovie.entities.UserEntity;
 public class UserFactory {
 	
 	public static UserEntity createUserEntity() {
-		UserEntity user = new UserEntity(2L, "Maria", "maria@gmail.com", "$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG");
+		UserEntity user = new UserEntity(2L, "Maria", "maria@gmail.com", "$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG");		
 		return user;
 	}
 }
